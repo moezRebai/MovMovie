21bc0d00d870298b95bccfd4a5a63d2dbb13011d
MRebai13K <moez.rebaii@gmail.com>
2018-05-27 14:35:09 +0200

    Add movie search bar ( by name and by gender)

--- MovMovie/MovMovie/Pages/Movies/Index.cshtml.cs
+++ MovMovie/MovMovie/Pages/Movies/Index.cshtml.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MovMovie.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MovMovie.Pages.Movies
 {
@@ -16,10 +18,30 @@ namespace MovMovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string movieGenre, string searchInfo)
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            if (!string.IsNullOrEmpty(searchInfo))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchInfo));
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
